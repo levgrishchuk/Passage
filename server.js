ec9075611af853825fafa842ec411d632254946a
moonwane <68343698+moonwane@users.noreply.github.com>
2022-04-10 00:01:15 -0400

    adding heroku hosting functionality

--- server.js
+++ server.js
@@ -41,14 +41,14 @@ app.use('/api/items', require('./routes/api/items'));
 //     app.use(path.join(__dirname, ''))
 // }
 
-const root = path.resolve(__dirname, './client/build')
+// const root = path.resolve(__dirname, './client/build')
 
 if (process.env.NODE_ENV === 'production') {
     // app.use(express.static("build"));
     //   app.get("*", (req, res) => {
     //     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
     //   });
-    app.use(express.static(root))
+    app.use(express.static("client/build"))
   } 
 
 // assign port number
